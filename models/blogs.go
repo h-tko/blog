0f573196eb69e27bb73a8be958e7c7b21f48f36c
h-tko <bojan_krkic_esp@i.softbank.jp>
2017-01-03 08:15:08 +0000

    find method add.

--- models/blogs.go
+++ models/blogs.go
@@ -22,12 +22,19 @@ func NewBlog() *Blog {
 }
 
 func (blog *Blog) FindById(id int) {
-	db.Where("is_show = ?", true).First(&blog, id)
+	db.Where("is_show = ?", true).Where("release_date <= now()").First(&blog, id)
+}
+
+func (blog *Blog) FindPopularList(limit int) []*Blog {
+	var blogs []*Blog
+	db.Where("is_show = ?", true).Where("release_date <= now()").Joins("inner join blog_counts on blogs.id = blog_counts.blog_id").Order("blog_counts.good_count").Find(&blogs)
+
+	return blogs
 }
 
 func (blog *Blog) FindList(limit int) []*Blog {
 	var blogs []*Blog
-	db.Where("is_show = ?", true).Order("release_date desc").Limit(limit).Find(&blogs)
+	db.Where("is_show = ?", true).Where("release_date <= now()").Order("release_date desc").Limit(limit).Find(&blogs)
 
 	return blogs
 }
